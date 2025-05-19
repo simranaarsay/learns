d94792ef315fc7100246cb9eb003cd4b3f2e70fa
simranaarsay <simranaarsay106@gmail.com>
2025-05-19 02:07:31 -0400

    created a bakery

--- dataStructure.js
+++ dataStructure.js
@@ -1,1 +1,37 @@
-//testing
+//
+//let's say i own a bakery
+
+
+//set price for each items.
+
+const itemsPrice =  {
+    bread: 4.23,
+    icecap: 2.00,
+    frappe: 4.00,
+    latte: 3.00,
+    brownbread: 7.00,
+    espresso: 1.23,
+}
+
+const tax = 0.07;
+
+let customerName = "janice";
+
+let amountJanice = (itemsPrice.bread + itemsPrice.espresso) ;
+let totalAmount = amountJanice * tax ;
+
+let totalWithTax =  amountJanice +totalAmount;
+
+console.log(amountJanice);
+
+console.log(totalAmount);
+
+console.log(totalWithTax);
+
+
+
+
+
+
+
+
