1228fcbd7b670d509d6e89d9b909316042432b15
simranaarsay <simranaarsay106@gmail.com>
2025-05-26 00:42:00 -0400

    update array.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+//console.log('Happy developing ✨')
+
+let language = "JavaScript";
+console.log(language);
+
+let code = 'K';
+let l = "learn" ;
+console.log('code', 'language', l );
+
